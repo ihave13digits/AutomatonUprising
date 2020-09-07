491a3ea31d15ba684f733de768ec0e40b1d4eeff
ihave13digits <ihave13digits@protonmail.com>
2020-09-07 14:50:51 -0400

    updated core

--- scene/World.gd
+++ scene/World.gd
@@ -20,11 +20,12 @@ func set_noise_values(n, sd, pd, oc, la, pr):
 	n.lacunarity = la
 	n.persistence = pr
 
-func spawn(t, pos):
+func spawn(t, pos, rot):
 	var target = Data.object[t]
 	var inst = target['instance'][0]
 	var I = load(Data.instance[inst]).instance()
 	I.translation = pos
+	I.rotation_degrees = rot
 	I.mesh['body'] = Data.object[t]['mesh'][0]
 	add_child(I)
 
@@ -36,17 +37,55 @@ func generate_world_data():
 			noise_moisture.get_noise_2d(x, z),
 			noise_temperature.get_noise_2d(x, z)
 			]
-	print(data)
 
 func generate_chunk(X, Z):
 	var chunk = Data.physics['chunk_size']
 	var size = Data.physics['tile_size']
-	var tile = 'tileflat'
+	
 	for x in range(chunk):
 		for z in range(chunk):
-			var y = data['%s-%s' % [x, z]][0] * Data.physics['max_height']
-			y = clamp(y, -Data.physics['max_height'], Data.physics['max_height'])
-			spawn(tile, Vector3((X*chunk)+(x*size), round(y), (Z*chunk)+(z*size)))
+			var Y = get_height(x, z)
+			var offset = Vector2(X*chunk, Z*chunk)
+			var tile
+			var rot = Vector3()
+			
+			var n = get_height(x+offset.x, z-1+offset.y)
+			var s = get_height(x+offset.x, z+1+offset.y)
+			var e = get_height(x-1+offset.x, z+offset.y)
+			var w = get_height(x+1+offset.x, z+offset.y)
+			var ne = get_height(x-1+offset.x, z-1+offset.y)
+			var nw = get_height(x+1+offset.x, z-1+offset.y)
+			var se = get_height(x-1+offset.x, z+1+offset.y)
+			var sw = get_height(x+1+offset.x, z+1+offset.y)
+			
+			if n and n > Y:
+				tile = 'tilesidecave'; rot.y = 0
+			elif s and s > Y:
+				tile = 'tilesidevex'; rot.y = 180
+			elif e and e > Y:
+				tile = 'tilesidecave'; rot.y = 90
+			elif w and w > Y:
+				tile = 'tilesidevex'; rot.y = 270
+			
+			elif ne and ne > Y:
+				tile = 'tilecave'; rot.y = 0
+			elif nw and nw > Y:
+				tile = 'tilevex'; rot.y = 180
+			elif se and se > Y:
+				tile = 'tilecave'; rot.y = 90
+			elif sw and sw > Y:
+				tile = 'tilevex'; rot.y = 270
+			
+			else:
+				tile = 'tileflat'; rot.y = 0
+			
+			spawn(tile, Vector3((X*chunk)+(x*size), Y, (Z*chunk)+(z*size)), rot)
+
+func get_height(x, z):
+	if '%s-%s' % [x, z] in data:
+		var y = data['%s-%s' % [x, z]][0] * Data.physics['max_height']
+		y = clamp(y, -Data.physics['max_height'], Data.physics['max_height'])
+		return round(y)
 
 func get_biome(v, t):
 	for b in Data.biome:
