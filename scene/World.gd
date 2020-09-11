d282211d869217bc108cb368f55688ac9c75a0c9
ihave13digits <ihave13digits@protonmail.com>
2020-09-11 00:00:15 -0400

    updated core

--- scene/World.gd
+++ scene/World.gd
@@ -11,6 +11,25 @@ var noise_temperature = OpenSimplexNoise.new()
 
 var data = {}
 
+var mask = {
+		'-1 -1 -1 0 0 0 0 0' : {'tile' : 'tilecave', 'rot' : 90},
+		'0 -1 -1 -1 0 0 0 0' : {'tile' : 'tileside', 'rot' : 90},
+		'0 0 -1 -1 -1 0 0 0' : {'tile' : 'tilecave', 'rot' : 180},
+		'0 0 0 -1 -1 -1 0 0' : {'tile' : 'tileside', 'rot' : 180},
+		'0 0 0 0 -1 -1 -1 0' : {'tile' : 'tilecave', 'rot' : 270},
+		'0 0 0 0 0 -1 -1 -1' : {'tile' : 'tileside', 'rot' : 270},
+		'-1 0 0 0 0 0 -1 -1' : {'tile' : 'tilecave', 'rot' : 0},
+		'-1 -1 0 0 0 0 0 -1' : {'tile' : 'tileside', 'rot' : 0},
+		'1 1 1 0 0 0 0 0' : {'tile' : 'tilevex', 'rot' : 270},
+		'0 1 1 1 0 0 0 0' : {'tile' : 'tileside', 'rot' : 270},
+		'0 0 1 1 1 0 0 0' : {'tile' : 'tilevex', 'rot' : 0},
+		'0 0 0 1 1 1 0 0' : {'tile' : 'tileside', 'rot' : 0},
+		'0 0 0 0 1 1 1 0' : {'tile' : 'tilevex', 'rot' : 90},
+		'0 0 0 0 0 1 1 1' : {'tile' : 'tileside', 'rot' : 90},
+		'1 0 0 0 0 0 1 1' : {'tile' : 'tilevex', 'rot' : 180},
+		'1 1 0 0 0 0 0 1' : {'tile' : 'tileside', 'rot' : 180},
+	}
+
 func _ready():
 	max_height = Data.physics['max_height']
 	world_size = Data.physics['world_size']
@@ -60,47 +79,35 @@ func generate_tile(x, z):
 	var s = get_height(x, z+1)
 	var e = get_height(x-1, z)
 	var w = get_height(x+1, z)
+	var ne = get_height(x-1, z-1)
+	var nw = get_height(x+1, z-1)
+	var se = get_height(x-1, z+1)
+	var sw = get_height(x+1, z+1)
+
+	if !n: n = 0
+	else: n -= Y
+	if !s: s = 0
+	else: s -= Y
+	if !e: e = 0
+	else: e -= Y
+	if !w: w = 0
+	else: w -= Y
+	if !ne: ne = 0
+	else: ne -= Y
+	if !nw: nw = 0
+	else: nw -= Y
+	if !se: se = 0
+	else: se -= Y
+	if !sw: sw = 0
+	else: sw -= Y
+
+	var nbrs = '%s %s %s %s %s %s %s %s' % [w, nw, n, ne, e, se, s, sw]
 	
-	if n and s and e and w:
-
-		if n == Y and s == Y and e == Y and w == Y:
-			tile = 'tileflat'; rot.y = 0
-
-		elif n < Y and s >= Y and e >= Y and w >= Y:
-			tile = 'tileside'; rot.y = 90
-		elif n >= Y and s < Y and e >= Y and w >= Y:
-			tile = 'tileside'; rot.y = 270
-		elif n >= Y and s >= Y and e < Y and w >= Y:
-			tile = 'tileside'; rot.y = 180
-		elif n >= Y and s >= Y and e >= Y and w < Y:
-			tile = 'tileside'; rot.y = 0
-
-#		elif n < Y and s >= Y and e < Y and w >= Y:
-#			tile = 'tilevert'; rot.y = 0
-#		elif n < Y and s >= Y and e >= Y and w < Y:
-#			tile = 'tilevert'; rot.y = 270
-#		elif n >= Y and s < Y and e < Y and w >= Y:
-#			tile = 'tilevert'; rot.y = 90
-#		elif n >= Y and s < Y and e <= Y and w < Y:
-#			tile = 'tilevert'; rot.y = 180
-
-		elif n < Y and s >= Y and e < Y and w >= Y:
-			tile = 'tilecave'; rot.y = 180
-		elif n < Y and s >= Y and e >= Y and w < Y:
-			tile = 'tilecave'; rot.y = 90
-		elif n >= Y and s < Y and e < Y and w >= Y:
-			tile = 'tilecave'; rot.y = 270
-		elif n >= Y and s < Y and e >= Y and w < Y:
-			tile = 'tilecave'; rot.y = 0
-
-		elif n <= Y and s > Y and e <= Y and w > Y:
-			tile = 'tilevex'; rot.y = 180
-		elif n <= Y and s > Y and e > Y and w <= Y:
-			tile = 'tilevex'; rot.y = 90
-		elif n > Y and s <= Y and e <= Y and w > Y:
-			tile = 'tilevex'; rot.y = 270
-		elif n > Y and s <= Y and e > Y and w <= Y:
-			tile = 'tilevex'; rot.y = 0
+	for b in mask:
+		match nbrs:
+			b :
+				tile = mask[b]['tile']
+				rot.y = mask[b]['rot']
 
 	spawn(tile, Vector3(x*tile_size, Y, z*tile_size), rot)
 
